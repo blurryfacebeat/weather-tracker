792952f35ac2de614c7a71c540d2be2aaa91d086
blurryfacebeat <blurryfacebeat@gmail.com>
2021-01-25 03:05:49 +0700

    deploy

---
+++ precache-manifest.e6df66f5a7999efcca389be3bf47aded.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ab8fddc37c0b5fe50d",
+    "url": "/weather-tracker/css/app.a22efa06.css"
+  },
+  {
+    "revision": "e98710e661c78d0c904f",
+    "url": "/weather-tracker/css/chunk-036ea6d0.19072002.css"
+  },
+  {
+    "revision": "f6892660291d7e69983c",
+    "url": "/weather-tracker/css/chunk-1f0edca0.c66e5a74.css"
+  },
+  {
+    "revision": "f7e21bc1039cbb5a7f72",
+    "url": "/weather-tracker/css/chunk-2fad549a.cb7b2160.css"
+  },
+  {
+    "revision": "52eba8e34b9f4786892a",
+    "url": "/weather-tracker/css/chunk-6401ce3a.7890f334.css"
+  },
+  {
+    "revision": "c0d0c11313ce22eb57cd",
+    "url": "/weather-tracker/css/chunk-e5b63602.dd5a03fe.css"
+  },
+  {
+    "revision": "99c04cd3072b89225e80",
+    "url": "/weather-tracker/css/chunk-f234896a.7890f334.css"
+  },
+  {
+    "revision": "a67651839b63d1132346768082d14110",
+    "url": "/weather-tracker/img/humidity.a6765183.svg"
+  },
+  {
+    "revision": "e61177a55b03974430e9c32b24029377",
+    "url": "/weather-tracker/img/pressure.e61177a5.svg"
+  },
+  {
+    "revision": "3ecd2f1603658abf34f6233466044e0d",
+    "url": "/weather-tracker/img/temperature.3ecd2f16.svg"
+  },
+  {
+    "revision": "330e5376c8144c0e7910df3441e45cc0",
+    "url": "/weather-tracker/img/wind.330e5376.svg"
+  },
+  {
+    "revision": "4d852a7ac7f72c1da7df1a99365ec011",
+    "url": "/weather-tracker/index.html"
+  },
+  {
+    "revision": "d7ab8fddc37c0b5fe50d",
+    "url": "/weather-tracker/js/app.06689ace.js"
+  },
+  {
+    "revision": "e98710e661c78d0c904f",
+    "url": "/weather-tracker/js/chunk-036ea6d0.57f5aa6e.js"
+  },
+  {
+    "revision": "f6892660291d7e69983c",
+    "url": "/weather-tracker/js/chunk-1f0edca0.9a461778.js"
+  },
+  {
+    "revision": "f7e21bc1039cbb5a7f72",
+    "url": "/weather-tracker/js/chunk-2fad549a.93d5f066.js"
+  },
+  {
+    "revision": "52eba8e34b9f4786892a",
+    "url": "/weather-tracker/js/chunk-6401ce3a.9d3f3602.js"
+  },
+  {
+    "revision": "c0d0c11313ce22eb57cd",
+    "url": "/weather-tracker/js/chunk-e5b63602.5b641d26.js"
+  },
+  {
+    "revision": "99c04cd3072b89225e80",
+    "url": "/weather-tracker/js/chunk-f234896a.cc57a01b.js"
+  },
+  {
+    "revision": "88eedd638ea3f410a390",
+    "url": "/weather-tracker/js/chunk-vendors.8e200777.js"
+  },
+  {
+    "revision": "b0eb4cc301285ed14e5ec2d215ac7030",
+    "url": "/weather-tracker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weather-tracker/robots.txt"
+  }
+]);
